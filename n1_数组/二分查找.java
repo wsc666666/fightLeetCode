418a72e7e8ce1dd34ee466a7d476d3aa05981655
Wangsc <1987620197@qq.com>
2023-09-20 23:32:14 +0800

    二分查找

--- n1_数组/二分查找.java
+++ n1_数组/二分查找.java
@@ -11,7 +11,7 @@ class 二分查找 {
         public int search(int[] nums, int target) {
             int left = 0;
             int right = nums.length-1;
-            while (left<right) {
+            while (left<=right) {
                 int middle = (left + right)/2;
                 if(target==nums[middle]){
                     return middle;
